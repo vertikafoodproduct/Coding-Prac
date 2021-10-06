7c417597dadce86b82bcc75b8cb7a497a85131c2
vertikafoodproduct <73388888+vertikafoodproduct@users.noreply.github.com>
2021-10-06 15:59:18 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   int year;
+   printf("Enter a year: ");
+   scanf("%d", &year);
+
+  
+   else {
+      printf("%d is not a leap year.", year);
+   }
+
+   return 0;
+}
